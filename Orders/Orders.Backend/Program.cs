b402269f78d3d8d4c52937d0bf5bdde72398b065
Manu Diaz <manu_04_04_92@hotmail.com>
2024-09-04 01:15:34 -0300

    Add Migrations and Create Dababase

---
+++ Orders/Orders.Backend/Program.cs
@@ -0,0 +1,29 @@
+using Microsoft.EntityFrameworkCore;
+using Orders.Backend.Data;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+//configuramos la inyeccion del DataContext
+builder.Services.AddDbContext<DataContext>(x=>x.UseSqlServer("name=LocalConnection"));
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
